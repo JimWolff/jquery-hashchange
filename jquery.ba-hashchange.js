0ce8444f1ffcca78a498ffebfbaff2fa439e92ad
Jim Wolff <frossen123@gmail.com>
2013-09-02 15:06:31 +0200

    replaced deprecated $.browser.msie with method that doesnt resort to useragent sniffing, ref: http://stackoverflow.com/questions/14892095/browser-msie-error-after-update-to-jquery-1-9-1/14892171#14892171

--- jquery.ba-hashchange.js
+++ jquery.ba-hashchange.js
@@ -297,7 +297,41 @@
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     // vvvvvvvvvvvvvvvvvvv REMOVE IF NOT SUPPORTING IE6/7/8 vvvvvvvvvvvvvvvvvvv
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    $.browser.msie && !supports_onhashchange && (function(){
+
+    var ie = (function () {
+      // ----------------------------------------------------------
+      // A short snippet for detecting versions of IE in JavaScript
+      // without resorting to user-agent sniffing
+      // ----------------------------------------------------------
+      // If you're not in IE (or IE version is less than 5) then:
+      //     ie === undefined
+      // If you're in IE (>=5) then you can determine which version:
+      //     ie === 7; // IE7
+      // Thus, to detect IE:
+      //     if (ie) {}
+      // And to detect the version:
+      //     ie === 6 // IE6
+      //     ie > 7 // IE8, IE9 ...
+      //     ie < 9 // Anything less than IE9
+      // ----------------------------------------------------------
+
+      // http://stackoverflow.com/questions/14892095/browser-msie-error-after-update-to-jquery-1-9-1/14892171#14892171
+
+        var undef,
+            v = 3,
+            div = document.createElement('div'),
+            all = div.getElementsByTagName('i');
+
+        while (
+            div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+            all[0]
+        );
+
+        return v > 4 ? v : undef;
+
+    }());
+
+    ie < 9 && !supports_onhashchange && (function(){
       // Not only do IE6/7 need the "magical" Iframe treatment, but so does IE8
       // when running in "IE7 compatibility" mode.
       
